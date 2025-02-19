28e8316279dbd8712feaf7bc6567a0eba4c52c47
Ashish Kacha <ashish.k.php@gmail.com>
2025-02-19 11:02:12 +0530

    Initial commit - React To-Do List App

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-scroll';
+
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="hero" smooth={true} duration={500}>Home</Link></li>
+        <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+        <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
+        <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <section id="hero" style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <h1>Hello, I'm Ashish Kacha</h1><p>I'm a Web Developer specializing in React, WordPress, and Shopify.</p>
+    </section>
+  );
+}
+
+export default Hero;
