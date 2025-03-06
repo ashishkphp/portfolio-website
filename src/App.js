763862f98d933231db0b0c0dec1c75eaeb631950
ashishkphp <ashish.k.php@gmail.com>
2025-03-06 18:27:55 +0530

    Update App.js
    For view dashboard on home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-// src/App.js
-import React from 'react';
-import Dashboard from './components/Dashboard';
+import React from "react"; // Add this line
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
 
-const App = () => {
+function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
-};
+}
 
 export default App;
