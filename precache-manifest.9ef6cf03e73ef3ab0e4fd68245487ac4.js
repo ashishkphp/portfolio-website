b461d6985c96c90598fbd80d1e1c42b74e23ccf4
Ashish Kacha <ashish.k.php@gmail.com>
2025-03-06 17:48:57 +0530

    Updates

---
+++ precache-manifest.9ef6cf03e73ef3ab0e4fd68245487ac4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "022d25fe27f4a465a2abbbec0056bdbc",
+    "url": "/ashishkphp/portfolio-website/index.html"
+  },
+  {
+    "revision": "0a788392b2ad8cf44076",
+    "url": "/ashishkphp/portfolio-website/static/css/main.43394c0f.chunk.css"
+  },
+  {
+    "revision": "2a4d06f5e1fd32baa7cc",
+    "url": "/ashishkphp/portfolio-website/static/js/2.d81dcac8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/ashishkphp/portfolio-website/static/js/2.d81dcac8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67067fc03640ad4e8cbd",
+    "url": "/ashishkphp/portfolio-website/static/js/3.a8f2faa3.chunk.js"
+  },
+  {
+    "revision": "0a788392b2ad8cf44076",
+    "url": "/ashishkphp/portfolio-website/static/js/main.5bba3edd.chunk.js"
+  },
+  {
+    "revision": "9aad6b1f022b91c2a425",
+    "url": "/ashishkphp/portfolio-website/static/js/runtime-main.66986792.js"
+  }
+]);
